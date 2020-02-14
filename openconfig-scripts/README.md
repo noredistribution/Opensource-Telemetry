886a2bec630153c603194293834d7798d7dc5c66
tamas <tjplug144@gmail.com>
2020-02-14 12:58:03 +0000

    add restconf scripts

---
+++ openconfig-scripts/README.md
@@ -0,0 +1,1 @@
+## Repo for OpenConfig/gNMI/RESTCONF/NETCONF script examples
---
+++ openconfig-scripts/restconf_get_hostname.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+USER = 'cvpadmin'
+PASS = 'arastra'
+
+requests.packages.urllib3.disable_warnings()
+
+api_call = "https://10.83.13.132:6020/restconf/data/system/config"
+
+headers = {'Content-Type': 'application/yang-data+json', 'Accept': 'application/yang-data+json'}
+
+result = requests.get(api_call, auth=HTTPBasicAuth(USER, PASS), headers=headers, verify=False)
+
+
+if result.status_code == 200:
+    	print ('Restconf call successful! Status Code is: {}\n').format(result.status_code)
+    	result = json.loads(result.content.decode('utf-8'))
+        print result
+else:
+    	print ('The Restconf call was not successful. Status Code is: {}').format(result.status_code)
